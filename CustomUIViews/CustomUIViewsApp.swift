7d5c7a5c1f69bb7dda4bd05faa5ffa89c9a78ec3
JaejunShin1996 <jaeshinkorean@gmail.com>
2022-11-08 19:14:40 +1030

    Long press button with gauge added.

---
+++ CustomUIViews/CustomUIViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomUIViewsApp.swift
+//  CustomUIViews
+//
+//  Created by Jaejun Shin on 8/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct CustomUIViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
