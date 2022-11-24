7fc5e59271ea2d28dcad958b47d1b28dd6abcf3d
JaejunShin1996 <jaeshinkorean@gmail.com>
2022-11-24 18:44:17 +1030

    Custom ViewModifer

---
+++ AdvancedSwiftUI/AdvancedSwiftUI/AdvancedSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedSwiftUIApp.swift
+//  AdvancedSwiftUI
+//
+//  Created by Jaejun Shin on 24/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct AdvancedSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CustomViewModifierExample()
+        }
+    }
+}
---
+++ AdvancedSwiftUI/AdvancedSwiftUI/CustomViewModifierExample.swift
@@ -0,0 +1,48 @@
+//
+//  CustomViewModifierExample.swift
+//  AdvancedSwiftUI
+//
+//  Created by Jaejun Shin on 24/11/2022.
+//
+
+import SwiftUI
+
+struct TextViewModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.title)
+            .frame(height: 66)
+            .frame(maxWidth: .infinity)
+            .background(.blue)
+            .cornerRadius(10.0)
+            .shadow(radius: 10.0)
+            .padding()
+    }
+}
+
+extension View {
+    func defaultTextViewFormatting() -> some View {
+        modifier(TextViewModifier())
+    }
+}
+
+struct CustomViewModifierExample: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+                .defaultTextViewFormatting()
+
+            Text("Hello, world!")
+                .defaultTextViewFormatting()
+
+            Text("Hello, world!")
+                .defaultTextViewFormatting()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomViewModifierExample()
+    }
+}
